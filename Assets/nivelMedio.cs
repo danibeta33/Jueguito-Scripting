9751838e00122c33913a9e6a19530f48f85b4775
luly903 <juliana.monroy@upb.edu.co>
2025-04-25 12:28:20 -0500

    gregar pantallas "ajustes", "dificultad" y "logros"

---
+++ Assets/nivelMedio.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using MoreMountains.Tools;
+using UnityEngine;
+
+public class nivelMedio : MonoBehaviour
+{
+    public void cambiarMedio()
+    {
+        MMSceneLoadingManager.LoadScene("nivel 3 dificil");
+    }
+}
