26731bd12e87449d9f031f4c0e71f43c5bede726
luly903 <juliana.monroy@upb.edu.co>
2025-05-16 09:00:17 -0500

    agragar cambios de escena, agregar pantalla logros

---
+++ Assets/Scripts/CambioEscena.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using MoreMountains.Tools;// Required to change scenes
+
+public class CambioEscena : MonoBehaviour
+{
+    public string sceneName; // Scene name to load, set in the Inspector
+
+    // This function will be called when the button is pressed
+    public void LoadScene()
+    {
+        MMSceneLoadingManager.LoadScene(sceneName);
+    }
+}
